adb43045c3127e269eadef2778815f11ab9f411c
yangyao <873241789@qq.com>
2019-02-12 09:49:11 +0800

    封装 header

---
+++ src/component/Header.js
@@ -0,0 +1,146 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019/2/11
+ */
+
+function headerInnerHtml() {
+    var address = '浦东新区，芳华路139号';
+    var email = 'shangfudata@com';
+    var phone = '666666666';
+    return '    <header class="header-area">\n' +
+        '        <!-- Top Header Area -->\n' +
+        '        <div class="top-header-area">\n' +
+        '            <div class="container h-100">\n' +
+        '                <div class="row h-100 align-items-center">\n' +
+        '                    <div class="col-12 d-flex justify-content-between">\n' +
+        '                        <!-- Logo Area -->\n' +
+        '                        <div class="logo">\n' +
+        '                            <a href="index.html"><img src="src/img/core-img/logo.png" alt=""></a>\n' +
+        '                        </div>\n' +
+        '                        <!-- Top Contact Info -->\n' +
+        '                        <div class="top-contact-info d-flex align-items-center">\n' +
+        '                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="' + address + '"><img src="src/img/core-img/placeholder.png" alt=""> <span>' + address + '</span></a>\n' +
+        '                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="' + email + '"><img src="src/img/core-img/message.png" alt=""> <span>' + email + '</span></a>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <!-- Navbar Area -->\n' +
+        '        <div class="credit-main-menu" id="sticker">\n' +
+        '            <div class="classy-nav-container breakpoint-off">\n' +
+        '                <div class="container" style="padding:0">\n' +
+        '                    <!-- Menu -->\n' +
+        '                    <nav class="classy-navbar justify-content-between" id="creditNav">\n' +
+        '                        <!-- Navbar Toggler -->\n' +
+        '                        <div class="classy-navbar-toggler">\n' +
+        '                            <span class="navbarToggler"><span></span><span></span><span></span></span>\n' +
+        '                        </div>\n' +
+        '                        <!-- Menu -->\n' +
+        '                        <div class="classy-menu">\n' +
+        '                            <!-- Close Button -->\n' +
+        '                            <div class="classycloseIcon">\n' +
+        '                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>\n' +
+        '                            </div>\n' +
+        '                            <!-- Nav Start -->\n' +
+        '                            <div class="classynav">\n' +
+        '                                <ul>\n' +
+        '                                    <li><a href="index.html">首页</a></li>\n' +
+        '                                    <li><a href="about.html">企业介绍</a></li>\n' +
+        '                                    <li><a href="services.html">产品介绍</a></li>\n' +
+        '                                    <li><a href="services.html">行业解决方案</a></li>\n' +
+        '                                    <li><a href="services.html">产品定价</a></li>\n' +
+        '                                    <li><a href="#">开发者中心</a>\n' +
+        '                                        <div class="megamenu">\n' +
+        '                                            <ul class="single-mega cn-col-4">\n' +
+        '                                                <li><a href="#">Portfolio 1</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 2</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 3</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 4</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 5</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 6</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 7</a></li>\n' +
+        '                                            </ul>\n' +
+        '                                            <ul class="single-mega cn-col-4">\n' +
+        '                                                <li><a href="#">Portfolio 8</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 9</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 10</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 11</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 12</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 13</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 14</a></li>\n' +
+        '                                            </ul>\n' +
+        '                                            <ul class="single-mega cn-col-4">\n' +
+        '                                                <li><a href="#">Portfolio 15</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 16</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 17</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 18</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 19</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 20</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 21</a></li>\n' +
+        '                                            </ul>\n' +
+        '                                        </div>\n' +
+        '                                    </li>\n' +
+        '                                    <li><a href="#">帮助中心</a>\n' +
+        '                                        <div class="megamenu">\n' +
+        '                                            <ul class="single-mega cn-col-4">\n' +
+        '                                                <li><a href="#">Portfolio 1</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 2</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 3</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 4</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 5</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 6</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 7</a></li>\n' +
+        '                                            </ul>\n' +
+        '                                            <ul class="single-mega cn-col-4">\n' +
+        '                                                <li><a href="#">Portfolio 8</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 9</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 10</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 11</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 12</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 13</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 14</a></li>\n' +
+        '                                            </ul>\n' +
+        '                                            <ul class="single-mega cn-col-4">\n' +
+        '                                                <li><a href="#">Portfolio 15</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 16</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 17</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 18</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 19</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 20</a></li>\n' +
+        '                                                <li><a href="#">Portfolio 21</a></li>\n' +
+        '                                            </ul>\n' +
+        '                                        </div>\n' +
+        '                                    </li>\n' +
+        '                                    <li><a href="post.html">博客</a></li>\n' +
+        '                                    <li><a href="contact.html">反馈</a></li>\n' +
+        '                                    <li><a href="#">Pages</a>\n' +
+        '                                        <ul class="dropdown">\n' +
+        '                                            <li><a href="index.html">Home</a></li>\n' +
+        '                                            <li><a href="about.html">About Us</a></li>\n' +
+        '                                            <li><a href="services.html">Services</a></li>\n' +
+        '                                            <li><a href="post.html">Post</a></li>\n' +
+        '                                            <li><a href="single-post.html">Single Post</a></li>\n' +
+        '                                            <li><a href="contact.html">Contact</a></li>\n' +
+        '                                            <li><a href="elements.html">Elements</a></li>\n' +
+        '                                        </ul>\n' +
+        '                                    </li>\n' +
+        '                                </ul>\n' +
+        '                            </div>\n' +
+        '                            <!-- Nav End -->\n' +
+        '                        </div>\n' +
+        '                        <!-- Contact -->\n' +
+        '                        <div class="contact">\n' +
+        '                            <a href="#"><img src="src/img/core-img/call2.png" alt=""> ' + phone + '</a>\n' +
+        '                        </div>\n' +
+        '                    </nav>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </header>';
+}
+
+function HeaderTemplate() {
+    return generateTemplate(headerInnerHtml());
+}
