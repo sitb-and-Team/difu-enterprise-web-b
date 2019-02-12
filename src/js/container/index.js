352a49f8677516695ffa9e809bd23076c2d8d4d7
yangyao <873241789@qq.com>
2019-02-12 09:49:33 +0800

    拆分js

---
+++ src/js/container/index.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019/2/11
+ */
+
+$('#preloader').html(PreloaderTemplate());
+$('#header').html(HeaderTemplate());
